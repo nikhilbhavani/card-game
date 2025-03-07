84bf2eab0045ed2b85eb1e17128f8224b80d05bc
Nikhil Bhavani <86593547+nikhilbhavani@users.noreply.github.com>
2025-03-07 17:57:46 -0500

    made changes to app.jsx

---
+++ src/data.jsx
@@ -0,0 +1,44 @@
+const data = [
+    {
+      id: 1,
+      isClicked: false,
+    },
+    {
+      id: 2,
+      isClicked: false,
+    },
+    {
+      id: 3,
+      isClicked: false,
+    },
+    {
+      id: 4,
+      isClicked: false,
+    },
+    {
+      id: 5,
+      isClicked: false,
+    },
+    {
+      id: 6,
+      isClicked: false,
+    },
+    {
+      id: 7,
+      isClicked: false,
+    },
+    {
+      id: 8,
+      isClicked: false,
+    },
+    {
+      id: 9,
+      isClicked: false,
+    },
+    {
+      id: 10,
+      isClicked: false,
+    }
+  ];
+  
+  export default data;
